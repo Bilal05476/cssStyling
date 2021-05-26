5afc986e08e1c60a04680890069f3dcf097df545
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-05-26 17:38:23 +0500

    updates

---
+++ Cert Generator/index.js
@@ -0,0 +1,22 @@
+const generatePDF = async (name) => {
+  const { PDFDocument, rgb } = PDFLib;
+  const exBytes = await fetch("./Certificate.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  const PdfDoc = await PDFDocument.load(exBytes);
+  const pages = PdfDoc.getPages();
+  const firstPg = pages[0];
+
+  firstPg.drawText(name, {
+    x: 279,
+    y: 312,
+    size: 40,
+    color: rgb(0.2, 0.4, 0.7),
+  });
+
+  const uri = await PdfDoc.saveAsBase64({ dataUri: true });
+  document.querySelector("#certificate").src = uri;
+};
+
+generatePDF("Bilal Ahmed");
